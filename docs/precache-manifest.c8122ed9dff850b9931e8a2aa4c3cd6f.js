cc5708e4c4c95eb39162d370c83622eb6cb062e7
Huynh Duc Toan <toanhd@cardano-labo.com>
2018-10-26 16:10:19 +0700

    first html build

---
+++ docs/precache-manifest.c8122ed9dff850b9931e8a2aa4c3cd6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "348b9dca4bb454fb1ae1",
+    "url": "/static/js/main.348b9dca.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "348b9dca4bb454fb1ae1",
+    "url": "/static/css/main.3f496d81.chunk.css"
+  },
+  {
+    "revision": "735e84fae8d9fa91ee8a39baa1c10c16",
+    "url": "/index.html"
+  }
+];
